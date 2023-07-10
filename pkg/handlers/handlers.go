3e62157c7c8c2dd687a703c08bca54b721c28c13
AqibNiazi <aqibjaved5201@gmail.com>
2023-07-10 17:32:41 +0500

    optimizing template

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,8 +2,27 @@ package handlers
 
 import (
 	"net/http"
+	"webapplication/pkg/config"
 	"webapplication/pkg/render"
 )
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.Appconfig
+}
+
+// NewRepo creates a new Repository
+func NewRepo(a *config.Appconfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandler
+func NewHandlers(r *Repository) {
+	Repo = r
+}
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
