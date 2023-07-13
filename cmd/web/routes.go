b3ad30150fdfb9b20a96a711f4d3985dae282f5d
AqibNiazi <aqibjaved5201@gmail.com>
2023-07-13 17:01:08 +0500

    routes with chi router is added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,12 +5,17 @@ import (
 	"webapplication/pkg/config"
 	"webapplication/pkg/handlers"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.Appconfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
